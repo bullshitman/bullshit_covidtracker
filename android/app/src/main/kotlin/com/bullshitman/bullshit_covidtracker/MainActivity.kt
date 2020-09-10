adfc9cfe87d5e7ef23d798bcd4a2582f5d46693a
BullshitMan <sokol.joe@gmail.com>
2020-09-10 23:34:04 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/bullshitman/bullshit_covidtracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bullshitman.bullshit_covidtracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/Tracker.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Tracker extends StatefulWidget {
+  @override
+  _TrackerState createState() => _TrackerState();
+}
+
+class _TrackerState extends State<Tracker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
